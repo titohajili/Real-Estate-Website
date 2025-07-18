c984eb18adebdba14d97c5318ed23c0d4aabebf6
titohajili <maqatoto963@gmail.com>
2025-07-18 23:11:20 +0400

    Real-Estate-Website

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const font = Roboto({
+  weight: ["100", "300","400","500", "700", "900"],
+  subsets: ["latin"],
+});
+
+
+
+export const metadata: Metadata = {
+  title: "Real Estate Project | Next.js",
+  description: "Real Estate Project",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className} >
+        {children}
+      </body>
+    </html>
+  );
+}
