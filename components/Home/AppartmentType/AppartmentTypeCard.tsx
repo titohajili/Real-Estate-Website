c832196a70d9bfae195282291ce799bf7e4ad1ce
titohajili <maqatoto963@gmail.com>
2025-10-04 18:11:20 +0400

    Appartment Section

---
+++ components/Home/AppartmentType/AppartmentTypeCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+type Props = {
+    type: {
+        id: number;
+        icon: string;
+        type: string;
+        number: number;
+    }
+}
+const AppartmentTypeCard = ({type}:Props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AppartmentTypeCard
