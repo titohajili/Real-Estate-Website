37a73b39b9aedd0846f7c39aa7ec717e7e3451af
titohajili <maqatoto963@gmail.com>
2025-11-08 13:45:59 +0400

    Building Feature Card

--- components/Home/BuildingFeature/BuildingFeature.tsx
+++ components/Home/BuildingFeature/BuildingFeature.tsx
@@ -11,7 +11,7 @@ const BuildingFeature = () => {
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 items-center mt-14'>
             {buildings.map((building)=>{
                 return <div key={building.id}>
-                    <BuildingFeatureCard  />
+                    <BuildingFeatureCard building={building}  />
                 </div>
             })}
         </div>
